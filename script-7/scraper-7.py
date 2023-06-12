de4d481923c4f61f8b3219bf84a472298517d1fd
Kaushal Mehta <kaushalkumar304@gmail.com>
2023-06-12 14:16:06 +0530

    updating scraper data

--- script-7/scraper-7.py
+++ script-7/scraper-7.py
@@ -8,6 +8,11 @@ import os
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
+# piyush shubham director
+# soumya prohram manager
+
+# ankes ritik raj member
+
 load_dotenv()
 
 DB_URL = os.environ.get('DB_URL')
@@ -24,9 +29,8 @@ logging.basicConfig(filename="scraper1", filemode='a',
 
 options = uc.ChromeOptions()
 options.add_argument('--headless')
-options.add_argument("User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
-
-
+options.add_argument(
+    "User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
 
 driver = uc.Chrome(version_main=114, options=options)
 # driver = webdriver.Chrome(options=options)
@@ -34,14 +38,16 @@ driver.maximize_window()
 # driver.get("https://attorneys.superlawyers.com/personal-injury-plaintiff/new-york/new-york/?page=1")
 # elem = driver.find_elements(By.CLASS_NAME, "lawyer-info-container > section a")
 
-with open("batch-1.json", 'r') as file:
+with open("batch-7.json", 'r') as file:
     firm_urls = json.load(file)
 
 lawyers = []
 
 RESTART_TIME = 2
+
 PAGE_NUM = 0
 LAWYER_DATA = 0
+
 
 def init():
     if os.path.exists('script_running'):
@@ -54,7 +60,7 @@ def init():
         lawyer_data_num = re.findall("LAWYER_DATA:(\d*)", page_info)
         if len(lawyer_data_num) >= 1:
             lawyer_data_num = lawyer_data_num[0]
-        
+
         global PAGE_NUM
         global LAWYER_DATA
 
@@ -62,10 +68,8 @@ def init():
         LAWYER_DATA = int(lawyer_data_num) if lawyer_data_num else 0
 
 
-
 def scrape_lawyers_links():
     init()
-
 
     for i in range(PAGE_NUM, len(firm_urls)):
         print(f"Scraping.... page {i}/{len(firm_urls)}", firm_urls[i])
@@ -80,33 +84,35 @@ def scrape_lawyers_links():
 
         try:
             top_spots_el = driver.find_element(By.ID, "topspots_wrapper")
-            top_spots_el = top_spots_el.find_elements(By.CLASS_NAME, 'full-name.fw-bold.mb-0.text-center.text-lg-start .directory_profile')
+            top_spots_el = top_spots_el.find_elements(By.CLASS_NAME,
+                                                      'full-name.fw-bold.mb-0.text-center.text-lg-start .directory_profile')
             top_spots_el = [top_spot.get_attribute('href') for top_spot in top_spots_el]
             lawyers_urls = [*top_spots_el, *lawyers_urls]
         except NoSuchElementException as e:
             print(e.msg)
-        
+
         try:
             spotlights_el = driver.find_element(By.ID, "spotlights_wrapper")
-            spotlights_el = spotlights_el.find_elements(By.CLASS_NAME, 'full-name.fw-bold.mb-0.text-center.text-lg-start .directory_profile')
+            spotlights_el = spotlights_el.find_elements(By.CLASS_NAME,
+                                                        'full-name.fw-bold.mb-0.text-center.text-lg-start .directory_profile')
             spotlights_el = [spotlight.get_attribute('href') for spotlight in spotlights_el]
             lawyers_urls = [*lawyers_urls, *spotlights_el]
         except NoSuchElementException as e:
             print(e.msg)
-        
+
         try:
             poap_results_el = driver.find_element(By.ID, "poap_results")
-            poap_results_el = poap_results_el.find_elements(By.CLASS_NAME, 'full-name.fw-bold.mb-0.text-center.text-lg-start .directory_profile')
+            poap_results_el = poap_results_el.find_elements(By.CLASS_NAME,
+                                                            'full-name.fw-bold.mb-0.text-center.text-lg-start .directory_profile')
             poap_results_el = [poap_result.get_attribute('href') for poap_result in poap_results_el]
             lawyers_urls = [*lawyers_urls, *poap_results_el]
         except NoSuchElementException as e:
             print(e.msg)
 
         scrape_lawyer_data(lawyers_urls, i)
-    
+
 
 def scrape_lawyer_data(urls, page_num):
-    
     global LAWYER_DATA
 
     for i in range(LAWYER_DATA, len(urls)):
@@ -152,26 +158,26 @@ def scrape_lawyer_data(urls, page_num):
                 lawyer_name = lawyer_name.text
         except NoSuchElementException as e:
             print(e.msg)
-        
+
         try:
             lawyer_phone = driver.find_element(By.CLASS_NAME, 'profile-phone-header.single-link')
             if lawyer_phone:
                 lawyer_phone = lawyer_phone.get_attribute("text")
         except NoSuchElementException as e:
             print(e.msg)
-        
+
         try:
             element = driver.find_element(By.ID, 'about')
             bio = element.text
         except NoSuchElementException as e:
             print(e.msg)
-        
+
         try:
             element = driver.find_element(By.ID, 'languages')
             languages = element.text
         except NoSuchElementException as e:
             print(e.msg)
-        
+
         try:
             element = driver.find_element(By.ID, 'licensed_since')
             licensed_since = element.text
@@ -179,13 +185,14 @@ def scrape_lawyer_data(urls, page_num):
             print(e.msg)
 
         try:
-            firm_name = driver.find_element(By.CLASS_NAME, 'profile-info-card .profile-profile-header.d-none.d-xl-block.paragraph-large-xl.mb-0.mb-xl-3')
+            firm_name = driver.find_element(By.CLASS_NAME,
+                                            'profile-info-card .profile-profile-header.d-none.d-xl-block.paragraph-large-xl.mb-0.mb-xl-3')
             if firm_name:
                 firm_name = firm_name.get_attribute('text')
         except NoSuchElementException as e:
             print(e.msg)
 
-        #profile switch tab
+        # profile switch tab
         try:
             element = driver.find_element(By.ID, 'practice-areas')
             driver.execute_script("arguments[0].setAttribute('class','tab-pane fade active show')", element)
@@ -205,11 +212,11 @@ def scrape_lawyer_data(urls, page_num):
                     link = el.find_element(By.TAG_NAME, 'a')
                     if link:
                         link = link.get_attribute('href')
-                    
-                    title = el.find_element(By.CLASS_NAME ,"sm-name")
+
+                    title = el.find_element(By.CLASS_NAME, "sm-name")
                     if title:
                         title = title.text
-            # social_links = [{'link': element.find_element(By.TAG_NAME ,"a").get_attribute('href'), 'title': element.find_element(By.CLASS_NAME ,"sm-name").text} for element in elements]
+                    # social_links = [{'link': element.find_element(By.TAG_NAME ,"a").get_attribute('href'), 'title': element.find_element(By.CLASS_NAME ,"sm-name").text} for element in elements]
                     social_links.append({'link': link, 'title': title})
             # print(social_links)
         except NoSuchElementException as e:
@@ -225,8 +232,6 @@ def scrape_lawyer_data(urls, page_num):
         except NoSuchElementException as e:
             print(e.msg)
 
-
-        
         # achievements tab switch
         try:
             element = driver.find_element(By.ID, 'achievements')
@@ -236,8 +241,8 @@ def scrape_lawyer_data(urls, page_num):
 
             for lecture_el in lectures_elements:
                 if lecture_el:
-                    lectures.append({'value': lecture_el.text, 'title': 'lectures/writings'})      
-            # lectures = [{'value':lecture_el.text, 'title': "lectures/writings"} for lecture_el in lectures]
+                    lectures.append({'value': lecture_el.text, 'title': 'lectures/writings'})
+                    # lectures = [{'value':lecture_el.text, 'title': "lectures/writings"} for lecture_el in lectures]
         except NoSuchElementException as e:
             print(e.msg)
 
@@ -249,8 +254,8 @@ def scrape_lawyer_data(urls, page_num):
 
             for community_service_el in community_service:
                 if community_service_el:
-                    community_service.append(community_service_el.text)              
-            # community_service = [community_service_el.text for community_service_el in community_services]
+                    community_service.append(community_service_el.text)
+                    # community_service = [community_service_el.text for community_service_el in community_services]
         except NoSuchElementException as e:
             print(e.msg)
 
@@ -259,16 +264,15 @@ def scrape_lawyer_data(urls, page_num):
             driver.execute_script("arguments[0].setAttribute('class','tab-pane fade active show')", element)
             industry_group_element = driver.find_element(By.ID, 'industry_groups')
             industry_group_elements = industry_group_element.find_elements(By.CLASS_NAME, 'list-spaced-small li')
-            
+
             for industry_group_el in industry_group_elements:
                 if industry_group_el:
                     industry_groups.append(industry_group_el.text)
 
-
             # industry_groups = [industry_group_el.text for industry_group_el in industry_groups]
         except NoSuchElementException as e:
             print(e.msg)
-        
+
         try:
             element = driver.find_element(By.ID, 'achievements')
             driver.execute_script("arguments[0].setAttribute('class','tab-pane fade active show')", element)
@@ -282,7 +286,7 @@ def scrape_lawyer_data(urls, page_num):
             # awards = [{'value':awards_el.text, 'title': "awards"} for awards_el in awards]
         except NoSuchElementException as e:
             print(e.msg)
-        
+
         try:
             element = driver.find_element(By.ID, 'achievements')
             driver.execute_script("arguments[0].setAttribute('class','tab-pane fade active show')", element)
@@ -300,7 +304,7 @@ def scrape_lawyer_data(urls, page_num):
             educations = [education_el.text if education_el else "N/A" for education_el in educations_element]
         except NoSuchElementException as e:
             print(e.msg)
-        
+
         try:
             element = driver.find_element(By.ID, 'achievements')
             driver.execute_script("arguments[0].setAttribute('class','tab-pane fade active show')", element)
@@ -309,7 +313,7 @@ def scrape_lawyer_data(urls, page_num):
             bar_activity = [bar_activity_el.text if bar_activity_el else "N/A" for bar_activity_el in bar_activity]
         except NoSuchElementException as e:
             print(e.msg)
-        
+
         try:
             element = driver.find_element(By.ID, 'achievements')
             driver.execute_script("arguments[0].setAttribute('class','tab-pane fade active show')", element)
@@ -336,7 +340,6 @@ def scrape_lawyer_data(urls, page_num):
             white_paper = [white_paper_el.text if white_paper_el else "N/A" for white_paper_el in white_papers]
         except NoSuchElementException as e:
             print(e.msg)
-        
 
         try:
             element = driver.find_element(By.ID, 'achievements')
@@ -346,30 +349,30 @@ def scrape_lawyer_data(urls, page_num):
             achievements = [achievements_el.text if achievements_el else "N/A" for achievements_el in achievements]
         except NoSuchElementException as e:
             print(e.msg)
-        
+
         data = {
-            "lawyer_name":lawyer_name,
-            "profile_image":profile_image,
-            "lawyer_phone":lawyer_phone,
-            "practise_areas":practise_areas,
-            "social_links":social_links,
-            "selections":selections,
-            "bio":bio,
-            "lectures":lectures,
-            "awards":awards,
-            "clients":clients,
-            "educations":educations,
-            "bar_activity":bar_activity,
-            "verdicts":verdicts,
-            "white_paper":white_paper,
-            "achievements":achievements,
-            "firm_name":firm_name,
-            "licensed_since":licensed_since,
-            "languages":languages,
+            "lawyer_name": lawyer_name,
+            "profile_image": profile_image,
+            "lawyer_phone": lawyer_phone,
+            "practise_areas": practise_areas,
+            "social_links": social_links,
+            "selections": selections,
+            "bio": bio,
+            "lectures": lectures,
+            "awards": awards,
+            "clients": clients,
+            "educations": educations,
+            "bar_activity": bar_activity,
+            "verdicts": verdicts,
+            "white_paper": white_paper,
+            "achievements": achievements,
+            "firm_name": firm_name,
+            "licensed_since": licensed_since,
+            "languages": languages,
             "industry_groups": industry_groups,
-            "community_service":community_service
+            "community_service": community_service
         }
-        
+
         saved_data = collection.insert_one(data)
         logging.info(f"Saved to DB...  url={urls[i]}")
         print(f"Saved to DB...  url={urls[i]}")
